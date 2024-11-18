306b282eeff223d5daf8046af9834dcdcb11c62e
lucas.romaniello <lucas.romaniello@squad.com.ar>
2024-11-18 06:27:33 -0300

    Spring security en pruebasVehiculos funcionando correctamente

--- vehiculos/pruebasVehiculos/src/main/java/tp/vehiculos/AppConfig.java
+++ vehiculos/pruebasVehiculos/src/main/java/tp/vehiculos/AppConfig.java
@@ -2,7 +2,20 @@ package tp.vehiculos;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +23,54 @@ public class AppConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                // Esta ruta puede ser accedida por cualquier ROL
+                .requestMatchers("/api/posicion/**")
+                .permitAll()
+
+                .requestMatchers("/api/vehiculos/**")
+                .permitAll()
+
+                // Esta ruta puede ser accedida por Administradores
+                .requestMatchers("/api/reportes/**")
+                .hasAuthority("ADMIN")
+
+                .anyRequest()
+                .authenticated()
+
+        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {}
+
+    @Bean
+    AuthoritiesConverter realmRolesAuthoritiesConverter() {
+        return claims -> {
+            var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
+            var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
+            List<GrantedAuthority> list = roles.map(List::stream)
+                    .orElse(Stream.empty())
+                    .map(SimpleGrantedAuthority::new)
+                    .map(GrantedAuthority.class::cast)
+                    .toList();
+
+            return list;
+        };
+    }
+
+    @Bean
+    JwtAuthenticationConverter authenticationConverter(
+            Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter) {
+        var authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            return authoritiesConverter.convert(jwt.getClaims());
+        });
+        return authenticationConverter;
+    }
+
 }
 
